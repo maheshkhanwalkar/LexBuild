8fe70f16102fb214b0ba0c2424af53bf48c943dc
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2017-12-19 22:02:23 -0500

    Fix NFAGen to use new Digraph API

--- Regex/NFAGen.cpp
+++ Regex/NFAGen.cpp
@@ -3,16 +3,20 @@
 
 std::unique_ptr<NFA> NFAGen::makeSimple(const std::string& regex)
 {
-	auto result = std::make_unique<NFA>(NFA(0));
-	int count = 0;
+	auto result = std::make_unique<NFA>();
+	int first = result->make_vertex();
+
+	result->set_start(first);
 
 	for(size_t s = 0; s < regex.size(); s++)
 	{
-		result->add_edge(count, count + 1, {regex[s], Edge_Type::NORMAL});
-		count++;
+		int second = result->make_vertex();
+		result->add_edge(first, second, NFAWeight(WeightType::NORMAL, regex[s]));
+
+		first = second;
 	}
 
-	result->add_accept(count);
+	result->add_accept(first);
 	return result;
 }
 
@@ -28,14 +32,14 @@ void NFAGen::makeOr(NFA& a, NFA& b)
 	int prev = a.get_start();
 
 	a.set_start(n_start);
-	a.add_edge(n_start, prev, {0, Edge_Type::EPSILON});
+	a.add_edge(n_start, prev, NFAWeight(WeightType::EPSILON, 0));
 
 	a.merge_at(b, n_start);
 
 	int n_accept = a.make_vertex();
 
 	for(int vertex : a.get_accept())
-		a.add_edge(vertex, n_accept, {0, Edge_Type::EPSILON});
+		a.add_edge(vertex, n_accept, NFAWeight(WeightType::EPSILON, 0));
 
 	a.clear_accept();
 	a.add_accept(n_accept);
@@ -49,16 +53,16 @@ void NFAGen::makeStar(NFA &a)
 	int prev = a.get_start();
 	a.set_start(n_start);
 
-	a.add_edge(n_start, prev, {0, Edge_Type::EPSILON});
-	a.add_edge(n_start, n_mid, {0, Edge_Type::EPSILON});
+	a.add_edge(n_start, prev, NFAWeight(WeightType::EPSILON, 0));
+	a.add_edge(n_start, n_mid, NFAWeight(WeightType::EPSILON, 0));
 
 	int n_accept = a.make_vertex();
 
-	a.add_edge(n_mid, prev, {0, Edge_Type::EPSILON});
-	a.add_edge(n_mid, n_accept, {0, Edge_Type::EPSILON});
+	a.add_edge(n_mid, prev, NFAWeight(WeightType::EPSILON, 0));
+	a.add_edge(n_mid, n_accept, NFAWeight(WeightType::EPSILON, 0));
 
 	for(int vertex : a.get_accept())
-		a.add_edge(vertex, n_mid, {0, Edge_Type::EPSILON});
+		a.add_edge(vertex, n_mid, NFAWeight(WeightType::EPSILON, 0));
 
 	a.clear_accept();
 	a.add_accept(n_accept);
